3a3b0ffbe68f1dec31932a0de92beead7b462a9a
10ap17 <panticaleksa1710@gmail.com>
2024-05-10 15:05:10 +0200

    added functions for adding patients and doctors

--- src/MedicalRecord.sol
+++ src/MedicalRecord.sol
@@ -1,27 +1,64 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.25;
 
 contract MedicalRecord{
 
-    struct Patients{
+    struct Patient{
         string name;
         bool gender;
         string homeAddress;
         uint256 age;
         uint256 height;
         uint256 weight;
-        string alergies;
+        string allergies;
         string contact;
         
     }
 
-    struct Doctors{
+    struct Doctor{
         string name;
         bool gender;
         string specialization;
         string contact;
         uint256 licenseId;
     }
+    mapping(address=>Patient) public patient;
+    mapping(address=>Doctor) public doctor;
+    uint256 public patientCounter;
+    uint256 public doctorCounder;
     
+
+    function addPatient(address _patientAddress, string memory _name,bool _gender,string memory _homeAddress,uint256 _age,uint256 _height,uint256 _weight,string memory _allergies,string memory _contact)external{
+         Patient memory newPatient = Patient({
+            name: _name,
+            gender: _gender,
+            homeAddress: _homeAddress,
+            age: _age,
+            height: _height,
+            weight: _weight,
+            allergies: _allergies,
+            contact: _contact
+        });
+
+        // Add the new patient to the mapping
+        patient[_patientAddress] = newPatient;
+        patientCounter++;
+    }
+
+    function addDoctor(address _doctorAddress, string memory _name, bool _gender, string memory _specialization,string memory _contact,uint256 _licenseId)external{
+         Doctor memory newDoctor = Doctor({
+            name: _name,
+            gender: _gender,
+            specialization: _specialization,
+            contact: _contact,
+            licenseId: _licenseId
+            
+        });
+
+        // Add the new patient to the mapping
+        doctor[_doctorAddress] = newDoctor;
+        doctorCounder++;
+    }
+
 }
